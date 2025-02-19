3586b3b653ae56a72c3bbaa9116c65f27257f226
ianrwan <ianwan012088@gmail.com>
2025-02-19 18:46:02 +0800

    update happy string comments

--- Leetcode/1415_The k-th Lexicographical String of All Happy Strings of Length n/main.c
+++ Leetcode/1415_The k-th Lexicographical String of All Happy Strings of Length n/main.c
@@ -4,6 +4,8 @@
 #include<string.h>
 
 /*
+思考方向:
+
 n 是得到 happy string 的長度
 k 是要得到的第幾個 happy string
 
@@ -19,6 +21,23 @@ k = 3, 第三個字組則為 "ba"
 5, A[1] = [ab, ac, ...]
 */
 
+/*
+combine function:
+
+1. input parameter:
+previous: 被合成的字串
+inputChar: 被合成的字元
+length: previous 字串長度
+
+2. output: 合成的字串
+
+將 previous 的字串與 inputChar 字元合併再一起
+例如:
+previous = "ab"
+inputChar = 'c'
+
+則 return "abc"
+*/
 char* combine(char* previous, char inputChar, int length)
 {
     int i = 0;
@@ -35,22 +54,24 @@ char* combine(char* previous, char inputChar, int length)
 
 char* getHappyString(int n, int k)
 {
-    int current1DLen = 0;
-    int previous1DLen = 0;
-    char **current = NULL;
-    char **previous = NULL;
+    int current1DLen = 0; // 存取目前的字串所需的陣列大小（亦即字串的所有排列組合）
+    int previous1DLen = 0; // 存取上一筆字串所需的陣列大小（亦即字串的所有排列組合）
+    char **current = NULL; // 存取目前的字串陣列
+    char **previous = NULL; // 存取上一筆的字串陣列
 
+    // 如果要求的 k 值超過 n 則回傳空字串
     if(k > 3*pow(2,n-1))
         return "";
 
     for(int i = 0 ; i < n ; i++)
-    {
-        previous1DLen = current1DLen;
-        current1DLen = 3*pow(2, i);
+    { 
+        previous1DLen = current1DLen; 
+        current1DLen = 3*pow(2, i); // i 個字元所有的排列組合
 
         previous = current;
-        current = (char**)malloc(sizeof(char*)*current1DLen);
+        current = (char**)malloc(sizeof(char*)*current1DLen); 
 
+        // 初始化
         if(i == 0)
         {
             current[0] = "a";
@@ -59,6 +80,8 @@ char* getHappyString(int n, int k)
         }
         else
         {
+            // 看上一筆字串有哪些字串, 並看每個字串最後一個字元決定最後一個字元的下一個字元要填的內容
+            // 如 previous = ["ab", "ac"...] 則 current = ["aba", "abc", "aca", "acb"...]
             for(int j = 0 ; j < previous1DLen ; j++)
             {   
                 if(previous[j][i-1] == 'a')
@@ -79,12 +102,6 @@ char* getHappyString(int n, int k)
                 }
             }
         }
-
-        for(int f = 0 ; f < current1DLen ; f++)
-        {
-            printf("%s\n", current[f]);
-        }
-        printf("\n");   
     }
     
     return current[k-1];
